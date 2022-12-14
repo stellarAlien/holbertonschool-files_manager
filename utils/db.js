6ed560f2d6cd6491a7ca5f6c0e17ec46d2960c92
khaledxab <khaledxab@gmail.com>
2022-12-14 16:46:14 +0100

    environment variables ✔

---
+++ utils/db.js
@@ -0,0 +1,9 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager_dev';
+
+class DBClient {
+  // DBClient
+}
