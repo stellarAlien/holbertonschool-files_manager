1d056187192b7ef44f64e555e91b13085cb710c3
Seif <saifgaida@gmail.com>
2022-12-17 21:12:13 +0100

    user registration module

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 import { dbClient } from "../utils/db";
+const crypto = require('crypto');
 
 dbclient = dbClient();
 
@@ -7,6 +8,18 @@ class UserController{
         const {email, password} = req.body;
         if(!email) res.status(400).json({error: 'Missing email'});
         if(!password) res.status(400).json({error: 'Missing password'});
-        if(dbClient.db.collection('users').find(email))
-    }
-}
+        usr = dbClient.db.collection('users').find(email).toArray((err, docs)=>{
+            if (err) { console.log(err); }
+            res.status(400).json({"error": "Already exist"});
+
+            hashed_password = crypto.createHash('sha1').update(password).digest(hex);
+
+            user = {'email': email, 'password': hashed_password};
+    
+            id = dbClient.db.collection('users').insertOne(user);
+            
+            res.status(201).json({"_id" : id,  "email": email, "password": hashed_password});
+            })}
+        };
+
+export {UserController as UserController};
