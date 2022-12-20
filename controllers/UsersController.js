d416d84f97add8bd60d6153d00d77b96070560a1
khaledxab <khaledxab@gmail.com>
2022-12-20 15:39:15 +0100

    Eslint ! you dont have power anymore xD

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,52 +8,52 @@ const sha1 = require('sha1');
 // POST /users should create a new user in DB
 export async function postNew(req, res) {
 
-    try {
-        const userEmail = req.body.email;
-        if (!userEmail) {
-            return res.status(400).send({
-                error: 'Missing email',
-            });
-        }
-
-        const userPassword = req.body.password;
-        if (!userPassword) {
-            return res.status(400).send({
-                error: 'Missing password',
-            });
-        }
-
-        let existingEmail = await dbClient.db.collection('users').findOne({ email: userEmail });
-        if (existingEmail) {
-            return res.status(400).send({
-                error: 'Already exist',
-            });
-        }
-
-        let userId;
-        const hashedPw = sha1(userPassword);
-        const newUser = {
-            email: userEmail,
-            password: hashedPw,
-        };
-
-        try {
-            await dbClient.db.collection('users').insertOne(newUser, (err) => {
-                userId = newUser._id;
-                return res.status(201).send({
-                    email: userEmail,
-                    id: userId,
-                });
-            });
-        } catch (err) {
-            return res.status(err.status).send({
-                'error': err,
-            });
-        }
-
-    } catch (error) {
-        return res.status(500).send({
-            error: 'Server error',
-        });
+  try {
+    const userEmail = req.body.email;
+    if (!userEmail) {
+      return res.status(400).send({
+        error: 'Missing email',
+      });
     }
+
+    const userPassword = req.body.password;
+    if (!userPassword) {
+      return res.status(400).send({
+        error: 'Missing password',
+      });
+    }
+    
+    let existingEmail = await dbClient.db.collection('users').findOne({ email: userEmail });
+    if (existingEmail) {
+      return res.status(400).send({
+        error: 'Already exist',
+      });
+    }
+
+    let userId;
+    const hashedPw = sha1(userPassword);
+    const newUser = {
+      email: userEmail,
+      password: hashedPw,
+    };
+
+    try {
+      await dbClient.db.collection('users').insertOne(newUser, (err) => {
+        userId = newUser._id;
+        return res.status(201).send({
+          email: userEmail,
+          id: userId,
+        });
+      });
+    } catch (err) {
+      return res.status(err.status).send({
+        'error': err,
+      });
+    }
+
+  } catch (error) {
+    return res.status(500).send({
+      error: 'Server error',
+    });
+  }
 }
