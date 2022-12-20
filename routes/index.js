84d9bb916a1dbdacab029f6424d085b66f3ae632
khaledxab <khaledxab@gmail.com>
2022-12-20 15:24:32 +0100

    routes fix try #1

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,9 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
-export default function Routes(app) {
-    const router = express.Router();
-    app.use('/', router);
+const router = express.Router();
 
-    router.get('/status', (req, res) => {
-        AppController.getStatus(req, res);
-    });
+router.get('/', AppController.index);
+router.post('/users', UsersController.create);
 
-    router.get('/stats', (req, res) => {
-        AppController.getStats(req, res);
-    });
-
-    router.post('/users', (req, res) => {
-        UsersController.postNew(req, res);
-    });
-}
+export default router;
