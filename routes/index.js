5a725cf52f2597a7b0b72be02a506a491a9e96cb
khaledxab <khaledxab@gmail.com>
2022-12-22 12:52:06 +0100

    add usercontroller to routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
@@ -10,7 +11,5 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 
 router.get('/users/me', UsersController.getMe);
-
-
 
 module.exports = router;
