a6ef70a6dc043182a8033042ff0377571ad4d477
stellarAlien <saifgaida@gmail.com>
2022-12-29 22:13:07 +0100

    post newfile route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FileController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -16,5 +17,7 @@ router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 
 router.get('/disconnect', AuthController.getDisconnect);
+
+router.post('/files', FileController.posUpload);
 
 module.exports = router;
